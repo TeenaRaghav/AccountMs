a285e0061d19176e07cd3b42bb8ca9a27a751a42
Teena R <TeenaR@tekarch.com>
2025-01-04 15:29:08 -0600

    added Repository, service and models files

--- src/main/java/com/tekarch/AccountMS/Controller/AccountController.java
+++ src/main/java/com/tekarch/AccountMS/Controller/AccountController.java
@@ -1,10 +1,14 @@
 package com.tekarch.AccountMS.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping ("/accounts")
 public class AccountController {
+    @Autowired
 
     @GetMapping("/account")
     public String getAccount(){
