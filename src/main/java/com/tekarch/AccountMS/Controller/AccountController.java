0149645209993074ae16ab069ca30c6554012cc4
Teena R <TeenaR@tekarch.com>
2025-01-05 13:50:20 -0600

    Change in Controller and model

--- src/main/java/com/tekarch/AccountMS/Controller/AccountController.java
+++ src/main/java/com/tekarch/AccountMS/Controller/AccountController.java
@@ -1,17 +1,49 @@
 package com.tekarch.AccountMS.Controller;
 
+import com.tekarch.AccountMS.Service.AccountServiceImpl;
+import com.tekarch.AccountMS.models.Accounts;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping ("/accounts")
 public class AccountController {
     @Autowired
+    private AccountServiceImpl accountService;
 
-    @GetMapping("/account")
-    public String getAccount(){
-        return "This is Account microservices";
+
+    @PostMapping
+    public ResponseEntity<Accounts> createAccount(@RequestBody Accounts account){
+        Accounts createdAccount = accountService.addAccount(account);
+        return new ResponseEntity<>(createdAccount,HttpStatus.CREATED);
     }
+
+    @GetMapping
+    public ResponseEntity<List<Accounts>>getAllAccounts(){
+    return new ResponseEntity<>(accountService.getAllAccounts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Accounts> getAccountById(@PathVariable Long id)
+    {
+     Accounts accounts = accountService.getAccountById(id);
+     if(accounts != null) {
+         return new ResponseEntity<>(accounts , HttpStatus.OK);
+     }
+     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteAccount(@PathVariable Long id){
+        if(!accountService.getAccountById(id).getAccountId().equals(0L)){
+            accountService.deleteAccount(id);
+        return ResponseEntity.ok("Account deleted successfully.");
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Account not found");
+    }
+
 }
